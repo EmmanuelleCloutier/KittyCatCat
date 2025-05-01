96b7a20aede6be664af0b1ec8d0d6f7f856446d7
EmmanuelleCloutier <darkforest1045@gmail.com>
2025-05-01 10:39:10 -0400

    fix souris bugs

--- scripts/Player/Souris.cs
+++ scripts/Player/Souris.cs
@@ -6,9 +6,11 @@ public partial class Souris : CharacterBody2D
 	[Export] public int Speed = 100;
 	private Vector2 targetPosition; // La position vers laquelle la souris va se déplacer
 	private RandomNumberGenerator rng = new RandomNumberGenerator(); // Générateur de nombres aléatoires
+	private AnimatedSprite2D animatedSprite;
 
 	public override void _Ready()
 	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		rng.Randomize(); 
 		MoveToRandomPosition();
 	}
@@ -26,12 +28,22 @@ public partial class Souris : CharacterBody2D
 		Velocity = direction * Speed;
 		
 		MoveAndSlide();
+		
+			// Retourne l'ennemi si nécessaire
+		if (direction.X < 0)
+		{
+			animatedSprite.Scale = new Vector2(-1, 1); // Retourne l'ennemi à gauche
+		}
+		else if (direction.X > 0)
+		{
+			animatedSprite.Scale = new Vector2(1, 1); // Retourne l'ennemi à droite
+		}
 	}
 
 	private void MoveToRandomPosition()
 	{
 		
-		targetPosition = new Vector2(rng.RandfRange(100, 500), rng.RandfRange(100, 300));
+		targetPosition = new Vector2(rng.RandfRange(25,525), rng.RandfRange(15, 230));
 	}
 
 	public void OnBodyEntered(Node body)
